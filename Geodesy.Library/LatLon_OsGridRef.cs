23a44273825511f3327b8d5f5c44c294f6ba97f2
jwmxyz <john@johnwmoody.co.uk>
2020-02-15 20:33:12 +0000

    Moved files

---
+++ Geodesy.Library/LatLon_OsGridRef.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geodesy.Library
+{
+    public class LatLon_OsGridRef : LatLonEllipsoidal
+    {
+        public LatLon_OsGridRef(double latitude, double longitude, double height) 
+            : base(latitude, longitude, height)
+        {
+
+        }
+    }
+}
