af571de1f5d9f74ad5aeead2da4c6fe5d5edc57f
jwmxyz <john@johnwmoody.co.uk>
2020-03-14 21:47:45 +0000

    Added comments

--- Geodesy.Library/Extensions/ConverstionExtensions.cs
+++ Geodesy.Library/Extensions/ConverstionExtensions.cs
@@ -1,21 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Geodesy.Library.Extensions
 {
     public static class ConverstionExtensions
     {
+        /// <summary>
+        /// Converts a int value into Radians
+        /// </summary>
+        /// <param name="val">The value that we want to convert into val</param>
+        /// <returns>The radians value that has been converted</returns>
         public static double ToRadians(this double val)
         {
             return Math.PI / 180 * val;
         }
 
+        /// <summary>
+        /// Converts a int value into Radians
+        /// </summary>
+        /// <param name="val">The value that we want to convert into val</param>
+        /// <returns>The radians value that has been converted</returns>
         public static double ToRadians(this int val)
         {
             return Math.PI / 180 * val;
         }
 
+        /// <summary>
+        /// Converts a value into degrees
+        /// </summary>
+        /// <param name="val">The value that we want to convert into degrees</param>
+        /// <returns>The value in degrees</returns>
         public static double ToDegrees(this double val)
         {
             return val * 180 / Math.PI;
